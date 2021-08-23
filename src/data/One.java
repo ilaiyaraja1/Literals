f6b69a1ea623fc7b1ab2660df160e3f3c2d0add7
ilaiyaraja1 <ilaiyaraja.bca@gmail.com>
2021-08-24 00:04:05 +0530

    Literals & importent codes
    Here we will discuss using short rout keys -new line ,tab,backslash,blank space

---
+++ src/data/One.java
@@ -0,0 +1,27 @@
+package data;
+
+public class One {
+	public static void main(String[] args) {
+         
+		
+		int a =100;
+  /*int is datatype
+   * a is variable
+   * 100 is literals    (or) otherwise we will call literals
+   */
+		System.out.println("Literals value is:"+a);
+    System.out.println("I am ailaiyaraja fromindia.\ncurrently i am living in Tamil\bnadu.\rand my motherlaguage is\tTamil.I love my county");		
+		
+	}
+
+     /*
+      *\n New Line
+      *\t horizontal tab
+      *\b blank space
+      *\a small beep
+      *\r carriage return
+      *\' single quote
+      *\" double quote
+	 */
+
+}
